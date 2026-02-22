d2585bd204925532302212179e3fcaf690620317
haruma1618 <jlin239@student.fuhsd.org>
2026-02-21 23:43:40 -0800

    Add difficulties + balancing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const GAMEVERSION = "dev-2.3.2"
+const GAMEVERSION = "dev-2.4.2"
 
 let clicker = document.getElementById("clicker");
 let clickerCountElem = document.getElementById("clicker-count");
@@ -19,8 +19,7 @@ class Upgrade {
 }
 
 class BasicUpgrade {
-    constructor(clicks, autoclicks, title, subtitle = "auto", cost = "auto") {
-        this.cost = cost === "auto" ? clicks * 100 + autoclicks * 60 : cost;
+    constructor(clicks, autoclicks, title, subtitle = "auto") {
         this.title = title;
         this.subtitle = subtitle === "auto" ? ((clicks === 0 ? "" : "+" + format(clicks) + " Saharsh Power\n") + (autoclicks === 0 ? "" : "+" + format(autoclicks) + " SPS")) : subtitle;
         this.clicks = clicks;
@@ -32,59 +31,64 @@ let upgrades = [
     new BasicUpgrade(0, 1, "Person of the day", "+1 SPS(Saharsh Per Second)"),
     new BasicUpgrade(1, 0, "Centrix81 Addiction", "+1 Saharsh Power"),
     new BasicUpgrade(0, 5, "SPED class", "+5 SPS(Saharsh Per Second)"),
-    new BasicUpgrade(0, 10, "Leaked yearbook photo", "+10 SPS(Saharsh Per Second)"),
+    new BasicUpgrade(5, 10, "Leaked yearbook photo"),
     new BasicUpgrade(25, 0, "2.5 GPA"),
     new BasicUpgrade(0, 70, "Gets hacked"),
-    new BasicUpgrade(100, 0, "70 IQ"),
+    new BasicUpgrade(70, 0, "70 IQ"),
     new BasicUpgrade(0, 190, "\"Below Average\" on CAST test"),
     new BasicUpgrade(200, 200, "2.0 GPA"),
     new BasicUpgrade(0, 674, "Leaked IP Address(205.173.47.249)"),
     new BasicUpgrade(706, 0, "Leaked Bio ID", "+K706 Saharsh Power where K is 1 and K706 is Saharsh's Bio ID"),
-    new BasicUpgrade(0, 1000, "Forgot discord account password"),
-    new BasicUpgrade(0, 1973, "Owner of 1973 Member Group Chat", "at Oct 31, 7:19 PM\n+1,973 SPS"),
-    new BasicUpgrade(3000, 0, "60 IQ"),
-    new BasicUpgrade(0, 5000, "50 IQ"),
+    new BasicUpgrade(0, 1500, "Forgot discord account password"),
+    new BasicUpgrade(1973, 0, "Owner of 1973 Member Group Chat", "at Oct 31, 7:19 PM\n+1,973 Saharsh Power"),
+    new BasicUpgrade(0, 4000, "60 IQ"),
+    new BasicUpgrade(4000, 0, "50 IQ"),
     new BasicUpgrade(7463, 0, "Owner of 7463 Member Group Chat", "at Nov 4, 7:48 AM\n+7,463 Saharsh Power"),
-    new BasicUpgrade(15000, 0, "1.5 GPA"),
-    new BasicUpgrade(10000, 10000, "Saharsh in 7th grade"),
-    new BasicUpgrade(20000, 20000, "1.0 GPA"),
+    new BasicUpgrade(0, 15000, "1.5 GPA"),
+    new BasicUpgrade(20000, 0, "1.0 GPA"),
     new BasicUpgrade(0, 49603, "Leaked Food Service Account Number"),
     new BasicUpgrade(51486, 0, "Leaked Insurance Plan ID: W0051486"),
-    new BasicUpgrade(100000, 0, "0.5 GPA"),
-    new BasicUpgrade(0, 100000, "40 IQ"),
-    new BasicUpgrade(300000, 0, "30 IQ"),
-    new BasicUpgrade(0, 300000, "0.0 GPA"),
-    new BasicUpgrade(500000, 500000, "Leaked Social Security Number"),
-    new BasicUpgrade(1000000, 0, "20 IQ"),
-    new BasicUpgrade(0, 1000000, "10 IQ"),
-    new BasicUpgrade(5000000, 5000000, "Skipped Tetanus, Diphtheria and Acellular Pertussis (Tdap) vaccine"),
+    new BasicUpgrade(70000, 70000, "Saharsh in 7th grade"),
+    new BasicUpgrade(0, 200000, "0.5 GPA"),
+    new BasicUpgrade(200000, 0, "40 IQ"),
+    new BasicUpgrade(0, 600000, "30 IQ"),
+    new BasicUpgrade(600000, 0, "0.0 GPA"),
+    new BasicUpgrade(2000000, 2000000, "Leaked Social Security Number"),
     new BasicUpgrade(5250578, 0, "Leaked ID Number"),
-    new BasicUpgrade(10000000, 10000000, "0 IQ"),
+    new BasicUpgrade(10000000, 10000000, "20 IQ"),
+    new BasicUpgrade(0, 30000000, "10 IQ"),
+    new BasicUpgrade(50000000, 50000000, "Skipped Tetanus, Diphtheria and Acellular Pertussis (Tdap) vaccine"),
+    new BasicUpgrade(0, 200000000, "0 IQ"),
+    new BasicUpgrade(300000000, 0, "Saharsh in 8th grade"),
     new BasicUpgrade(909343909, 909343909, "Leaked Insurance Member ID:", "TGX909343909\n+909,343,909 Saharsh Power\n+909,343,909 SPS"),
-    new BasicUpgrade(1000000000, 0, "Broken computer"),
-    new BasicUpgrade(0, 1000000000, "Person of the day II"),
-    new BasicUpgrade(2000000000, 0, "Centrix81 Addiction II"),
-    new BasicUpgrade(0, 2000000000, "SPED Class II"),
-    new BasicUpgrade(10000000000, 0, "Leaked yearbook photo II"),
-    new BasicUpgrade(0, 10000000000, "Get hacked II"),
-    new BasicUpgrade(100000000000, 0, "\"Below average\" on CAST test II"),
-    new BasicUpgrade(0, 100000000000, "Forgot Discord account password II"),
-    new BasicUpgrade(1000000000000, 0, "Skip Tdap vaccine II"),
-    new BasicUpgrade(10000000000000, 0, "Person of the day III"),
+    new BasicUpgrade(0, 5000000000, "Broken computer"),
+    new BasicUpgrade(5000000000, 0, "Person of the day II"),
+    new BasicUpgrade(0, 20000000000, "Centrix81 Addiction II"),
+    new BasicUpgrade(20000000000, 0, "SPED Class II"),
+    new BasicUpgrade(0, 100000000000, "Leaked yearbook photo II"),
+    new BasicUpgrade(100000000000, 0, "Get hacked II"),
+    new BasicUpgrade(0, 500000000000, "\"Below average\" on CAST test II"),
+    new BasicUpgrade(500000000000, 0, "Forgot Discord account password II"),
+    new BasicUpgrade(2000000000000, 0, "Skip Tdap vaccine II"),
+    new BasicUpgrade(0, 20000000000000, "Person of the day III"),
     new BasicUpgrade(100000000000000, 0, "Centrix81 Addiction III"),
-    new BasicUpgrade(0, 1000000000000000, "SPED Class III"),
+    new BasicUpgrade(0, 500000000000000, "SPED Class III"),
     new BasicUpgrade(1000000000000000, 1000000000000000, "Leaked yearbook photo III"),
     new BasicUpgrade(0, 10000000000000000, "Get hacked III"),
     new BasicUpgrade(10000000000000000, 0, "\"Below average\" on CAST test III"),
-    new BasicUpgrade(10000000000000000000, 0, "Forgot Discord Account password III"),
+    new BasicUpgrade(1000000000000000000, 0, "Forgot Discord Account password III"),
     new BasicUpgrade(0, 10000000000000000000, "Skip Tdap Vaccine III"),
-    new BasicUpgrade(1000000000000000000000000, 1000000000000000000000000, "Saharsh", "Final upgrade\n+10^24 Saharsh Power\n+10^24 SPS"),
+    new BasicUpgrade(100000000000000000000, 0, "-100 IQ"),
+    new BasicUpgrade(0, 500000000000000000000, "-4.0 GPA"),
+    new BasicUpgrade(100000000000000000000000, 100000000000000000000000, "Saharsh", "Final upgrade\n+10^23 Saharsh Power\n+10^23 SPS"),
 ]
 
 let shinyUpgrades = [
     new Upgrade("Shiny Saharsh Boost", (n)=>(`+50% SPS and Saharsh Power (Currently: +${n*50}%)`), (n)=>(10+10*n)),
     new Upgrade("Saharsh Finance", (n)=>(`Decrease the cost multiplier ${getCostMulti(gameState.ownedShinyUpgrades[1])} -> ${getCostMulti(gameState.ownedShinyUpgrades[1]+1)}`), (n)=>(10+10*n)),
-    new Upgrade("Saharsh Expertise", (n)=>(`+100% Saharsh XP generation (Currently: +${n*100}%)`), (n)=>(6+3*n))
+    new Upgrade("Saharsh Expertise", (n)=>(`+100% Saharsh XP generation (Currently: +${n*100}%)`), (n)=>(6+3*n)),
+    new Upgrade("Saharsh Crit Chance", (n)=>(`Every click has a chance of producing 25x Saharsh (${n*3}% -> ${(n+1)*3}%)`), (n)=>(8+8*n)),
+    new Upgrade("Shiny Saharsh Chance", (n)=>(`Increase the Shiny Saharsh click chance (1/${Math.round(512*0.75**n)} -> 1/${Math.round(512*0.75**(n+1))})`), (n)=>(10+6*n)),
 ]
 
 function resetGameState() {
@@ -95,6 +99,8 @@ function resetGameState() {
     gameState.ownedUpgrades = new Array(upgrades.length).fill(0);
     gameState.ownedShinyUpgrades = new Array(shinyUpgrades.length).fill(0);
     gameState.xpCount = 0;
+    gameState.startTime = Date.now();
+    gameState.difficulty = 0;
 }
 
 let gameState = {};
@@ -126,8 +132,20 @@ function format(num) {
     }
 }
 
+function formatHighPrec(num) {
+    if (num < 1e3) {
+        return num - Math.floor(num) === 0 ? num.toString() : num.toFixed(4);
+    } else if (num < 1e6) {
+        return num - Math.floor(num) === 0 ? num.toString() : num.toFixed(2);
+    } else if (num < 1e12) {
+        return Math.floor(num).toLocaleString();
+    } else {
+        return num.toExponential(6);
+    }
+}
+
 function getCostMulti(upg1) {
-    return 0.2*Math.pow(0.75, upg1)+1;
+    return 0.25*Math.pow(0.6, upg1)+1;
 }
 
 function getXpReq(lv) {
@@ -153,11 +171,21 @@ function getLevel() {
 }
 
 function getBoostFromLevel() {
-    return 1+Math.pow(getLevel(), 1.5)/100;
+    return 1+Math.pow(getLevel(), 1.25)/80;
 }
 
 function getUpgradeCost(i) {
-    return Math.ceil(upgrades[i].cost * getCostMulti(gameState.ownedShinyUpgrades[1]) ** gameState.ownedUpgrades[i]);
+    let cost = 0;
+    cost = 100*upgrades[i].clicks + 60*upgrades[i].autoclicks;
+
+    cost *= getCostMulti(gameState.ownedShinyUpgrades[1]) ** gameState.ownedUpgrades[i];
+    if (gameState.difficulty == 1) {
+        cost *= 3;
+    } else if (gameState.difficulty == 2) {
+        cost *= 5 * (2 + (Date.now()-gameState.startTime)/1000/240);
+    }
+
+    return Math.ceil(cost);
 }
 
 function getShinyCost() {
@@ -178,7 +206,7 @@ function updateUpgradeSection() {
         let cost = getUpgradeCost(i);
         let item = upgItems[i];
 
-        let isVisible = (i <= highestOwnedUpg || gameState.clickerCount >= upgrades[i - 1].cost);
+        let isVisible = (i <= highestOwnedUpg || gameState.clickerCount >= getUpgradeCost(i-1) || gameState.clickerCount >= getUpgradeCost(i));
 
         if (!isVisible) {
             item.innerHTML = `<pre class="upgrade-text"><strong>???</strong> <i class="small">(??? Saharsh)</i></pre>`;
@@ -192,12 +220,19 @@ function updateUpgradeSection() {
             }
             
             let desc = upgrade.subtitle ? upgrade.subtitle.replace(/\n/g, "<br>") : "";
+            if (gameState.difficulty === 1 && i === 0) {
+                desc += "<i>\n(All SPS is multiplied by 2 in Hard Mode)</i>";
+            }
+            if (gameState.difficulty === 2 && i === 0) {
+                desc += "<i>\n(All SPS is multiplied by 3 in Insane Mode)</i>";
+            }
+            
             item.innerHTML = `<pre class="upgrade-text"><strong>${upgrade.title}</strong> <i class="small">(${format(cost)} Saharsh)</i> <i class="small">(owned: ${num})</i>${desc ? "<p class=\"small\">" + desc + "</p>" : ""}</pre>`;
         }
     }
 
-    if ((gameState.prestigeCount > 0 || gameState.clickerCount >= 1e27)) {
-        prestigeBtn.innerHTML = `<pre class="upgrade-text"><strong>Saharsh Prestige</strong> <p class="small">Ascend to a higher Saharsh (+${getPendingPrestige()} Super Saharsh)</p></pre>`;
+    if ((gameState.prestigeCount > 0 || gameState.clickerCount >= 1e26 || gameState.ownedUpgrades.at(-1) > 0)) {
+        prestigeBtn.innerHTML = `<pre class="upgrade-text"><strong>Saharsh Prestige</strong> <p class="small">Ascend to a higher Saharsh (+${formatHighPrec(getPendingPrestige())} Super Saharsh)</p></pre>`;
     }
 }
 
@@ -244,7 +279,7 @@ function buyUpgrade(index) {
     if (gameState.clickerCount >= cost) {
         gameState.clickerCount -= cost;
         if (gameState.ownedUpgrades[index] == 0) {
-            gameState.xpCount += 30*Math.log10(10*upgrades[index].clicks + 6*upgrades[index].autoclicks) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
+            gameState.xpCount += 30*Math.log10(10*upgrades[index].clicks + 4*upgrades[index].autoclicks) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
         }
         gameState.ownedUpgrades[index]++;
     }
@@ -277,7 +312,29 @@ function getAutoPerSec() {
     autoPerSec *= (2+gameState.prestigeCount)**2/4 * (1 + gameState.ownedShinyUpgrades[0]*0.5) * getBoostFromLevel();
     autoPerSec *= getSlotsProdBoost();
     autoPerSec *= bonusMulti;
+    if (gameState.difficulty === 1) {
+        autoPerSec *= 2;
+    }
+    if (gameState.difficulty === 2) {
+        autoPerSec *= 3;
+    }
     return autoPerSec;
+}
+
+function getShinyChance() {
+    let c = 1/(512 * 0.75**gameState.ownedShinyUpgrades[4]);
+    if (currentSlotEffect === 3) {
+        c *= 25;
+    } else if (currentSlotEffect === 4) {
+        c *= 5;
+    }
+
+    if (gameState.difficulty === 1) {
+        c /= 2;
+    } else if (gameState.difficulty === 2) {
+        c /= 4;
+    }
+    return c;
 }
 
 function doubleOrNothing(currency) {
@@ -306,8 +363,8 @@ function doubleOrNothing(currency) {
 }
 
 function spinSlotMachine() {
-    if (gameState.shinyCount < 3) return;
-    gameState.shinyCount -= 3;
+    if (gameState.shinyCount < 2) return;
+    gameState.shinyCount -= 2;
 
     for (let slot of document.getElementsByClassName("slot")) {
         slot.innerHTML = "";
@@ -391,20 +448,24 @@ function getSlotsXpBoost() {
 }
 
 function click() {
-    gameState.clickerCount += getClickPower();
-    gameState.xpCount += (1+Math.log10(getClickPower())) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
+    let critRand = (Math.random() < gameState.ownedShinyUpgrades[3]*0.03);
+    let clickValue = critRand ? getClickPower()*25 : getClickPower();
+
+    gameState.clickerCount += clickValue;
+    gameState.xpCount += (1+Math.log10(clickValue)) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
 
     let numText = document.createElement("div");
     numText.classList.add("click-number-text");
-    numText.innerHTML = "+" + format(getClickPower());
+    numText.innerHTML = "+" + format(clickValue);
     numText.style.top = (window.innerHeight/2 + 20 + 20*(Math.random()-1/2)) + "px";
     numText.style.left = (window.innerWidth/2 - 15 + 200*(Math.random()-1/2)) + "px";
+    if (critRand) {numText.style.color = "#bb0000";}
     numText.addEventListener('animationend', numText.remove);
     document.querySelector("#click-number-texts").appendChild(numText);
 
-    if (Math.random() < (currentSlotEffect == 3 ? 25 : (currentSlotEffect == 4 ? 5 : 1))/512) {
+    if (Math.random() < getShinyChance()) {
         gameState.shinyCount++;
-        gameState.xpCount += 80*(1+Math.log10(getClickPower())) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
+        gameState.xpCount += 80*(1+Math.log10(clickValue)) * (1+gameState.ownedShinyUpgrades[2]) * getSlotsXpBoost();
 
         let numText = document.createElement("div");
         numText.classList.add("click-number-text");
@@ -515,6 +576,19 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
         for (let elem of document.querySelectorAll(".clicker-section")) {
             elem.style.display = document.getElementById(currClickerSection) === elem ? "flex" : "none";
+        }
+
+        document.getElementById("gambling-icon").style.backgroundColor = currentSlotEffect == 0 ? "#3a7fff" : "#ffbd3a";
+        document.getElementById("difficulty-button").innerHTML = "Difficulty: " + ["Normal", "Hard", "Insane"][gameState.difficulty];
+        if (gameState.difficulty == 0) {
+            document.getElementById("game-container").style.backgroundColor = "#ffffff";
+            document.getElementById("info-container").style.color = "#000000";
+        } else if (gameState.difficulty === 1) {
+            document.getElementById("game-container").style.backgroundColor = "#ffffff";
+            document.getElementById("info-container").style.color = "#600020";
+        } else {
+            document.getElementById("game-container").style.backgroundColor = "#000000";
+            document.getElementById("info-container").style.color = "#ffadc3";
         }
 
         gameState.clickerCount += getAutoPerSec()/60;
